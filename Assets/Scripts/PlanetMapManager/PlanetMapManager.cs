96acd71c305e68911b85f0f8bdf232459b23fdf3
JaroslavL <swtfustarkiller@gmail.com>
2015-01-22 23:48:18 +0400

    Добавлена сцена галактики Добавлена сцена карты планеты Добавлены PlanetManager, PlanetMapManager
    То есть появилась возможность выбрать планету и выбрать местность на ней

---
+++ Assets/Scripts/PlanetMapManager/PlanetMapManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetMapManager : MonoBehaviour {
+
+	public void LoadLevel ()
+	{
+		Debug.Log ("LoadLevel_1");
+		Application.LoadLevel ("testing");
+	}
+}
