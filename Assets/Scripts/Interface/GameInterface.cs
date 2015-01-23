f15664e4155a5430954d7b3ca2bc081aea84ecc3
Garret has koller <swtfustarkiller@gmail.com>
2015-01-23 14:09:37 +0300

    Добавлено:
    - Связка ЛаунчСкрина с ГелаксиСкрином
    - кнопка пауза
    - "выйти", "рефреш" уровня

---
+++ Assets/Scripts/Interface/GameInterface.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInterface : MonoBehaviour {
+
+	public GameObject _panel;
+
+	public void StopContinueGame ()
+	{
+		if (Time.timeScale == 0)
+		{
+			Time.timeScale = 1;
+			_panel.SetActive (false);
+		}
+		else
+		{
+			Time.timeScale = 0;
+			_panel.SetActive (true);
+		}
+	}
+
+	public void Quit ()
+	{
+		Time.timeScale = 1;
+		Application.LoadLevel ("Galaxy");
+	}
+
+	public void Refresh ()
+	{
+		Time.timeScale = 1;
+		Application.LoadLevel (Application.loadedLevelName);
+	}
+}
---
+++ Assets/Scripts/Launcher/Launch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LaunchGame ()
+	{
+		Debug.Log ("Launch");
+		Application.LoadLevel ("Galaxy");
+	}
+}
